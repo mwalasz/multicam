590b17592b07428b57776a21afabaa4493135477
mwalasz <mwalasz@interia.pl>
2021-04-07 13:46:42 +0200

    Removing frozen frames

--- hub/hub.py
+++ hub/hub.py
@@ -8,6 +8,11 @@ import numpy as np
 # number of clients in final stream change for parameters
 ROWS = 2
 COLS = 2
+CLIENTS_NUMBER = ROWS * COLS
+
+ACTIVITY_CHECK_PERIOD = 5
+# how often will the activity be checked
+ACTIVITY_CHECK_TIME = CLIENTS_NUMBER * ACTIVITY_CHECK_PERIOD
 
 def processImage(image):
     # somehow process image
@@ -16,9 +21,9 @@ def processImage(image):
 # frames from all clients
 frames = {}
 
-# clients
-last_active_client = {}
-last_active = datetime.now()
+# time when clients were last time active
+last_active_time = {}
+last_active_check = datetime.now()
 
 # hub for connection with client
 client = imagezmq.ImageHub()
@@ -28,24 +33,38 @@ server = imagezmq.ImageSender(connect_to = 'tcp://*:5566', REQ_REP = False)
 print("[INFO] Hub started!")
 while True:
     # receive image from client
-    client_name, frame = client.recv_image()
+    client_id, frame = client.recv_image()
     client.send_reply(b'OK')
 
-    if client_name not in last_active_client.keys():
-        print("[INFO] Receiving data from {}...".format(client_name))
+    if client_id not in last_active_time.keys():
+        print("[INFO] Receiving data from {}...".format(client_id))
     
-    last_active_client[client_name] = datetime.now()
+    last_active_time[client_id] = datetime.now()
 
     # prepare received frame
     frame = imutils.resize(frame, width=400)
     (h, w) = frame.shape[:2]
 
     # save to all frames
-    frames[client_name] = frame
+    frames[client_id] = frame
 
     # create montage from all frames
     montages = imutils.build_montages(frames.values(), (w, h), (ROWS, COLS))
     
     # send built montage
     for montage in montages:
-        server.send_image(client_name, montage)
+        server.send_image(client_id, montage)
+
+    # if current time *minus* last time when the active device check
+	# was made is greater than the threshold set then do a check
+    if (datetime.now() - last_active_check).seconds > ACTIVITY_CHECK_TIME:
+        print("[INFO] Activity check.")
+		# loop over all previously active devices
+        for (client_id, ts) in list(last_active_time.items()):
+            if (datetime.now() - ts).seconds > ACTIVITY_CHECK_TIME:
+                print("[INFO] lost connection to {}".format(client_id))
+                last_active_time.pop(client_id)
+                frames.pop(client_id) # remove frame from nonactive client
+                
+        # set the last active check time as current time
+        last_active_check = datetime.now()
