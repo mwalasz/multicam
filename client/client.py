0174217bc7812e7e25a79ee0fda441ee8c04c87f
Piotr Matras <piotrmatras21@gmail.com>
2021-04-19 22:10:56 +0200

    added arg for setting client name displayed in video stream, extended logging

--- client/client.py
+++ client/client.py
@@ -3,23 +3,27 @@ import time
 from imutils.video import VideoStream
 import imagezmq
 import argparse
+import logging
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-ip", "--serverip", default="127.0.0.1", help="ip address of streaming server")
+host_name = socket.gethostname()
+host_ip = socket.gethostbyname(host_name)
+ap.add_argument("-n", "--name", default="{}/{}".format(host_name, host_ip), help="Name of client which will be printed on streamed video")
 args = vars(ap.parse_args())
 
 # connection with hub
 sender = imagezmq.ImageSender(connect_to='tcp://{}:5555'.format(args["serverip"]))
+logging.basicConfig(format="[%(levelname)s][%(asctime)s][{}]: %(message)s".format(args["name"]), level=logging.INFO)
 
-host_name = socket.gethostname()
-host_ip = socket.gethostbyname(host_name)
-host_id = "[{}/{}]".format(host_name, host_ip)
-print("[INFO] Client started! Name: {}, IP: {}".format(host_name, host_ip))
+logging.info("Client started! Name: {}, IP: {}".format(args["name"], host_ip))
 
 stream = VideoStream().start()
+logging.info("Warming camera sensor")
 time.sleep(2.0)  # allow camera sensor to warm up
 
-print("[INFO] Streaming now...")
+logging.info("Starting stream...")
+video_watermark = "[{}]".format(args["name"])
 while True:
     frame = stream.read()
-    sender.send_image(host_id, frame)
+    sender.send_image(video_watermark, frame)
